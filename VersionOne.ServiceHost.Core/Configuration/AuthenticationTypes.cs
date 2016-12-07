92d9fb8e49fd382de27a52a4245d192360225177
darchitetto <denise@digital-pioneers.com>
2016-12-07 13:27:02 -0500

    S-60568 Remove the no longer relevent Authentication Enum values

--- VersionOne.ServiceHost.Core/Configuration/AuthenticationTypes.cs
+++ VersionOne.ServiceHost.Core/Configuration/AuthenticationTypes.cs
@@ -5,12 +5,6 @@ namespace VersionOne.ServiceHost.Core.Configuration
     public enum AuthenticationTypes
     {
         [XmlEnum("0")]
-        AccessToken = 0,
-        [XmlEnum("1")]
-        Basic,
-        [XmlEnum("2")]
-        Integrated,
-        [XmlEnum("3")]
-        IntegratedWithCredentials
+        AccessToken = 0
     }
 }
