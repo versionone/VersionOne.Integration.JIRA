c68143bf1685323d6d657653876a14058b793b78
Andrew Leytze <aleytze@gmail.com>
2020-10-13 13:26:11 -0400

    S-72107 - Adding assembly version and informational version to get version to show up when checking properties of the files.

--- VersionOne.ServiceHost/Properties/AssemblyInfo.cs
+++ VersionOne.ServiceHost/Properties/AssemblyInfo.cs
@@ -6,3 +6,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCompanyAttribute("Digital.ai Software, Inc.")]
 [assembly: AssemblyCopyrightAttribute("Copyright © 2020, Digital.ai Software, Inc.")]
 [assembly: ComVisibleAttribute(false)]
+
+[assembly: AssemblyVersion("8.0.55555.*")]
+[assembly: AssemblyInformationalVersion("8.0")]
